1c042cf777ca402dbe8485dc144de7d816fdf519
Skye McLean <skyemclean87@gmail.com>
2024-01-08 21:39:23 -0800

    Added Counters

--- src/App.js
+++ src/App.js
@@ -21,25 +21,39 @@ const GREETINGS = [
 
 const RainShop = () => {
   const [welcome, setWelcome] = useState('');
+  const [raincount, setRainCount] = useState(0);
+  const [shopcount, setShopCount] = useState(0);
 
   const handleWelcome = () => {
     const welcome = GREETINGS[Math.floor(Math.random() * GREETINGS.length)]
     setWelcome(welcome);
   }
 
-  useEffect(() => { 
+  const handleRainCount = () => {
+    setRainCount(raincount + 1)
+  };
+
+  const handleShopCount = () => {
+    setShopCount(shopcount + 1)
+  };
+
+  useEffect(() => {
     handleWelcome();
-}, []);
+  }, []);
 
   return (
     <div className='title'>
-      Rain Shop 
+      Rain Shop
       <div className="body-wrapper">
         <p className='welcome'>{welcome}</p>
         <header className="body">
-         <a title="Random Wiki Page" href="https://simple.wikipedia.org/wiki/Special:Random" target="_blank" rel="noreferrer"><button className="rain-btn">Rain 🌧️</button></a>
-          <a title="Random Amazon Product" href="https://imfeelingprimey.com" target="_blank" rel="noreferrer"><button className="shop-btn">Shop 💲</button></a>
+          <a title="Random Wiki Page" href="https://simple.wikipedia.org/wiki/Special:Random" target="_blank" rel="noreferrer"><button onClick={handleRainCount} className="rain-btn">Rain 🌧️</button></a>
+          <a title="Random Amazon Product" href="https://imfeelingprimey.com" target="_blank" rel="noreferrer"><button onClick={handleShopCount} className="shop-btn">Shop 💲</button></a>
         </header>
+        <div className='counter-wrapper'>
+          <div className='counter'>Rain: {raincount}</div>
+          <div className='counter' >Shop: {shopcount}</div>
+        </div>
       </div>
     </div>
   );
